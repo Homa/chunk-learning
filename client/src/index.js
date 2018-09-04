f5c578f87d164d040b26d44b542aeeff06955862
Homa <homa@homas-mbp-2.attlocal.net>
2018-09-03 22:14:12 -0400

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import App from './components/App'
+import { Provider } from 'react-redux'; // glue between redux and react
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers , {}, applyMiddleware());
+
+ReactDom.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
