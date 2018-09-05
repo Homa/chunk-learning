c395b590fe03e53e9d293ef58b0101b53c90e811
Homa <homa@homas-mbp-2.attlocal.net>
2018-09-05 18:42:07 -0400

    added redux-thunk and actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import "materialize-css/dist/css/materialize.min.css";
+import { connect } from 'react-redux';
+import * as actions from '../actions'
 
 const Header = () => {
   return(
@@ -15,22 +17,27 @@ const Header = () => {
   );
 }
 const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header></Header>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
-          <Route exact path="/surveyNew" component={SurveyNew}></Route>
-          <Route exact path="/" component={Landing}></Route>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header></Header>
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+            <Route exact path="/" component={Landing}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDom from 'react-dom';
 import App from './components/App'
 import { Provider } from 'react-redux'; // glue between redux and react
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
-const store = createStore(reducers , {}, applyMiddleware());
+const store = createStore(reducers , {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
