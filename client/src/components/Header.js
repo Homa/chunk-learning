7370666cabdca1c3bc8ea87f1fc87fba3151585a
Homa <homa@homas-mbp-2.attlocal.net>
2018-09-07 18:41:13 -0400

    refactoring...

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,28 +9,28 @@ class Header extends Component {
       case false:
         // not logged in
         return (
-          <li><a href="/auth/google">Login with Google</a></li>
+          <a href="/auth/google">Login with Google</a>
         );
       case null:
         // waithing for api call
         return;
       default:
-      return (
-        <li><a href="/api/logout">Log out</a></li>
-      );
+        return (
+          <a href="/api/logout">Log out</a>
+        );
     }
 
   }
   render() {
     return(
       <nav>
-          <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li>{this.renderContent()}</li>
-            </ul>
-          </div>
-        </nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Logo</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>{this.renderContent()}</li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
