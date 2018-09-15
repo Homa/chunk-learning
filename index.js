3b60febfca36a6555b3429b1b8d1b6577b993fa9
Homa <neda.homa@gmail.com>
2018-09-15 19:49:41 -0400

    express client static file serving and express client route handling

--- index.js
+++ index.js
@@ -29,5 +29,17 @@ mongoose.connect(keys.mongoURI).then(
   err => { console.log(err) }
 );
 
+if(process.env.NODE_ENV === 'production') {
+  // express serve static production assets
+  app.use(express.static('client/build'));
+
+  // if express doesn't recognize the routes,
+  // it serves up the index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT);
