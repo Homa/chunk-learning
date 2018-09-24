b332a593651e8f0a097d4fa5af6b585ef6d3e208
Homa <neda.homa@gmail.com>
2018-09-23 21:28:06 -0400

    fixed a bug

--- models/Deck.js
+++ models/Deck.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const cardSchema = require('Card');
+const cardSchema = require('./Card');
 
 const deckSchema = new Schema(
   {
